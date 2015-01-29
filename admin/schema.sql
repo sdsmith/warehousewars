9ce4a50afb2f734ddbc330dc9ed4436bfbce24ff
sdsmith <stewart.dryden.smith@gmail.com>
2015-01-29 15:51:05 -0500

    Updatw - more descriptive table names and attributes.

--- admin/schema.sql
+++ admin/schema.sql
@@ -1,18 +1,18 @@
-drop table userdata;
-drop table userstats;
+drop table appuser;
+drop table ww_appuser_stats;
 
-create table userdata (
-	userid BIGSERIAL PRIMARY KEY ON DELETE CASCADE,
-	username VARCHAR(20) NOT NULL UNIQUE,
+create table appuser (
+	id BIGSERIAL PRIMARY KEY ON DELETE CASCADE,
+	name VARCHAR(20) NOT NULL UNIQUE,
 	password VARCHAR(50) NOT NULL,
-	email VARCHAR(50) NOT NULL,
+	email VARCHAR(50) NOT NULL UNIQUE,
 	joindate TIMESTAMP NOT NULL,
 	validated BOOLEAN NOT NULL,
 	lastlogin TIMESTAMP NOT NULL;
 );
 
-CREATE TABLE userstats (
-	userid INT PRIMARY KEY REFERENCES userdata.userid,
+CREATE TABLE ww_appuser_stats (
+	userid INT PRIMARY KEY REFERENCES appuser.id,
 	score INT NOT NULL,
 	maxlevel INT,
 	kills INT,
