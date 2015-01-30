adf4050555913e1723871c0338ec85a25b5025a6
sdsmith <stewart.dryden.smith@gmail.com>
2015-01-29 23:19:35 -0500

    Added login capability, and reworked database table interaction to simplify early development.

--- admin/schema.sql
+++ admin/schema.sql
@@ -1,25 +1,31 @@
 DROP TABLE appuser;
+DROP TABLE appuser_passwords;
 DROP TABLE ww_appuser_stats;
 
-CREATE TABLE appuser (
-	id BIGSERIAL PRIMARY KEY ON DELETE CASCADE,
-	name VARCHAR(20) NOT NULL UNIQUE,
-	email VARCHAR(50) NOT NULL UNIQUE,
-	joindate TIMESTAMP NOT NULL,
-	validated BOOLEAN NOT NULL,
-	lastlogin TIMESTAMP NOT NULL;
-);
 
 CREATE TABLE appuser_passwords (
-	userid BIGINT PRIMARY KEY REFERENCES appuser.id,
-	password VARCHAR(50) NOT NULL
+	userid 		BIGINT UNIQUE,
+	password 	VARCHAR(50) 	NOT NULL,
+	PRIMARY KEY (userid)
 );
 
 CREATE TABLE ww_appuser_stats (
-	userid BIGINT PRIMARY KEY REFERENCES appuser.id,
-	score INT NOT NULL,
-	maxlevel INT,
-	kills INT,
-	deaths INT,
-	steps BIGINT
+	userid 		BIGINT,
+	gameid	 	INT,
+	score 		INT 	NOT NULL,
+	maxlevel 	INT,
+	kills 		INT,
+	deaths 		INT,
+	steps 		BIGINT,
+	PRIMARY KEY (userid, gameid)
+);
+
+CREATE TABLE appuser (
+	id 		BIGSERIAL,
+	name 		VARCHAR(20) 	NOT NULL 	UNIQUE,
+	email 		VARCHAR(50) 	NOT NULL 	UNIQUE,
+	joindate 	TIMESTAMP 	NOT NULL,
+	validated 	BOOLEAN 	NOT NULL,
+	lastlogin 	TIMESTAMP 	NOT NULL,
+	PRIMARY KEY (id)
 );
--- index.php
+++ index.php
@@ -5,21 +5,20 @@ session_start();
 $state = &$_SESSION['state'];
 $authenticated = &$_SESSION['authenticated'];
 $user_name = &$_SESSION['user_name'];
+$user_id = &$_SESSION['userid'];
 
 $errormessage = "";
 
 $dbconn = NULL;
 
 
-/* Open global connection to database */
 function dbConnect() {
 	global $dbconn;	
-	if (!isset(dbconn)) {
+	if (!isset($dbconn)) {
 		$dbconn = pg_connect("host=localhost dbname=smiths61 user=smiths61 password=59882") or die("Could not connect: " . pg_last_error());
 	}
 }
 
-/* Close global connection to database */
 function dbClose() {
 	global $dbconn;
 	if (isset($dbconn)) {
@@ -35,6 +34,7 @@ if (!isset($state)) {
 	$state = "home_guest";
 }
 
+
 // Determine current state to serve content
 $action = &$_POST['action'];
 switch ($state) {
@@ -43,17 +43,45 @@ switch ($state) {
 		$view = "home.php";
 		$authenticated = false;
 		
-		if ($action == 'login') {
-			// User is logging in; check validity.
+		if ($action == "login") {
+			// User is logging in; check credentials against the database.
 			$cred_username = $_POST['login_username'];
 			$cred_password = $_POST['login_password'];
 
 			dbConnect();
-			pg_prepare($dbconn, "credential_check", 'SELECT COUNT(*) FROM appuser as user, appuser_passwords as passwd WHERE user.name = $1 and passwd.password = $2');
-			$result = pg_execute($dbconn, "credential_check", array($cred_username, $cred_password));
+			// NOTE(sdsmith): Used delayed pg_prepare in future.
+			$prepare_ret = pg_prepare($dbconn, "credential_check", 'SELECT * FROM appuser, appuser_passwords WHERE appuser.name = $1 and appuser_passwords.password = $2');
+			if ($prepare_ret) {
+				$resultobj = pg_execute($dbconn, "credential_check", array($cred_username, $cred_password));
+				if ($resultobj) {
+					$result_as_array = pg_fetch_array($resultobj);
+					if ($result_as_array != false) {
+						// There was a result, so we are authenticated.
 
-			$resarr = pg_fetch_all($result);
-			echo $resarr;
+						// Sanity check: confirm there was only one credential match.
+						if ($result_as_array = pg_fetch_array($result)) {
+							$authenticated = false;
+							die("Multiple user credential matches");
+						}
+
+						// Properly authenticated; do post authentication setup below
+						$authenticated = true;
+						$state = "home_authenticated";
+						$user_id = $result_as_array['id'];
+						$user_name = $cred_username;
+
+					} else {
+						// Did not authenticate
+						$errormessage = "Invalid Credentials: " . pg_last_error();
+					}
+				} else {
+					die("Query failed: " . pg_last_error());
+				}
+			} else {
+				die("Prepared statement failed: " . pg_last_error());
+			}
+
+			dbClose();
 		}
 
 		break;
@@ -81,15 +109,21 @@ switch ($state) {
 		<h1>Warehouse Wars Online</h1>
 		<hr/>
 
+		<?php if ($errormessage != "") : ?>
+		<div id='errormessage'>
+			<?php echo $errormessage ?>
+		</div>
+		<?php endif ?>
+
 		<?php include($view) ?>
 	</body>
 </html>
 
 
-<?php
+<?php /*
 // This is 'just in case' code, to make sure that the database connection is
 // always closed on page exit.
 if (isset($dbconn)) {
 	pg_close($dbconn);
-}
+}*/
 ?>
