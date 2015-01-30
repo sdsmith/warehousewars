952deecc94f93309128020cedff08f8a3220dfd5
sdsmith <stewart.dryden.smith@gmail.com>
2015-01-30 18:07:59 -0500

    *Forgot to push register.php with last few commits

---
+++ register.php
@@ -0,0 +1,21 @@
+<div id="register">
+	<h3>Registration</h3>
+	
+	<form id="registration_form" method="post">
+		<input type="hidden" name="action" value="registration_submit" />
+		
+		Username: <input type="text" name="reg_username" 
+			value="<?php echo $_POST['reg_username']; ?>" /><br/>
+		Email: <input type="text" name="reg_email" 
+			value="<?php echo $_POST['reg_email']; ?>" /><br/>
+		Password: <input type="password" name="reg_password" /><br/>
+		Confirm Password: <input type="password" 
+			name="reg_confirm_password" /><br/>
+		<input type="submit" value="Submit" />
+	</form>
+
+	<form id="back" method="post">
+		<input type="hidden" name="action" value="home" />
+		<input type="submit" value="back" />
+	</form>
+</div>
