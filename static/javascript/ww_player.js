11bd830cb58dc2de67e8e14d2e2b8f09649c56e8
sdsmith <stewart.dryden.smith@gmail.com>
2015-02-04 18:19:13 -0500

    Added settings view (forgot too earlier). Modified spacing and variable names in ww_actor and finished ww_player. Linked javascript files to ww.html.

--- static/javascript/ww_player.js
+++ static/javascript/ww_player.js
@@ -2,12 +2,28 @@
 /*
  * Player constructor.
  */
-function Player(x, y, imgsrc) {
-	self._actor = new Actor(x, y, imgsrc);
+function Player(x, y) {
+	var image_source = ;
+	this._actor = new Actor(x, y, image_source);
 }
 
 Player.prototype.getPosition = function() {
-	return self._actor.getPosition();
+	return this._actor.getPosition();
 }
 
+Player.prototype.setPosition = function() {
+	return this._actor.setPosition();
+}
+
+Player.prototype.getImage = function() {
+	return this._actor.getImage();
+}
+
+Player.prototype.setImage = function(image_source) {
+	return this._actor.setImage(image_source);
+}
+
+Player.prototype.keyboardMove = function() {
+	return false;
+}
 /* END Class Player */
