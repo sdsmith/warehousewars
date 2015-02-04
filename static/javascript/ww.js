3309f8001c9413c885c284460f149ea094f8a71c
StefanLatychev <stefan.latychev@mail.utoronto.ca>
2015-02-04 13:59:16 -0500

    Added Actor functions

--- static/javascript/ww.js
+++ static/javascript/ww.js
@@ -90,16 +90,54 @@ Stage.prototype.getActor=function(x, y){
  * Actor constructor. Take stage position (x,y) and the source of the image to
  * be displayed in its position.
  */
-function Actor(x, y, imgsrc) {
+function Actor(x, y, imgsrc, delay=5) {
 	this.pos_x = x;
 	this.pos_y = y;
 	this.imgsrc = imgsrc;
-
+	this.delay_count = 0;
+	this.delay = delay;
 	
 }
+
+Actor.prototype.getPosition=function(){
+	return (this.pos_x, this.pos_y);
+}
+
+Actor.prototype.setPosition=function=(x, y){
+	this.pos_x = x;
+	this.pos_y = y;	
+}
+
+Actor.prototype.getImage=function(){
+	return this.imgsrc;
+}
+
+Actor.prototype.setImage=function(imgsrc){
+	this.imgsrc = imgsrc;
+}
+
+//Generic step function to be overridden by specific actors that will use it
+Actor.prototype.step=function(){
+	return true;
+}
+
+//Generic is_dead function to be overridden by specific actors
+Actor.prototype.is_dead(){
+	return false;
+}
+
+//Generic move function that moves the actor by dx, dy relative to their position
+Actor.prototype.move(dx, dy){
+	this.setPosition(this.pos_x + dx, this.pos_y + dy);
+	return true;
+}
+
+//Delay function that will allow for speed settings of different actors relative to the base delay amount.
+Actor.prototype.delay(){
+	this.delay_count = (this.delay_count + 1) % this.delay;
+	return this.delay_count == 0;
+}
 /* END Class Actor */
-
-
 
 
 
