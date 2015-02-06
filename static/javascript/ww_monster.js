b3878720a7cd84d42f23a34f7bfe8cef24aca84e
latyche1 <latyche1@dh2020pc03.erin.utoronto.ca>
2015-02-06 16:43:02 -0500

    Fixed monster tick with temporary fix using force_update

--- static/javascript/ww_monster.js
+++ static/javascript/ww_monster.js
@@ -40,16 +40,20 @@ Monster.prototype.setImage = function(image_source) {
  * Will check if monster is dead, will appropriately delay itself if need be,
  * and make itself move
  */
-Monster.prototype.tick = function() {
-
-	if(this.isDead){
-		this._stage.removeActor(this);
+Monster.prototype.tick = function(force_update) {
+	if(force_update) {
+	//TODO(SLatychev): force_update being used temporarily as it may not be called just on initialization later
+	/*NOTE(SLatychev): The current state of tick is jerry rigged (if it works at all) because without the force_update
+	 * check the tick will fall into the isDead code to try and extract a false result, unfortunately this doesn't occur
+	 * instead the monster while being initialized will think it is surrounded by default(check if this is actually true and 
+	 * not further broken logic) and mess up the spawn sequence
+	 */
+		if (this.isDead()) {
+			this._stage.removeActor(this);
+		} else if (!this._actor.delay()) {
+			return this.move(this.dx, this.dy);
+		}
 	}
-
-	if(!this._actor.delay()){
-		return this._actor.move(this.dx, this.dy);
-	}
-	
 	return false;
 }
 
@@ -66,16 +70,15 @@ Monster.prototype.isDead = function() {
 	for(var i = 0; i < delta.length; i++) {
 		for(var j = 0; j < delta.length; j++) {
 
-			var surroundCheck = this._stage.getActor(monster_pos[0] + delta[i], 
-																 monster_pos[1]+ delta[j]);
+			var surroundCheck = this._stage.getActor(monster_pos[0] + delta[i], monster_pos[1]+ delta[j]);
 			//Individual square check
 			if(surroundCheck !== null) {
 				counter += 1;
 
-				if(counter === 8)	{
+				if(counter === 8) {
 					this.deathCheck += 1;
 					//Checking for death ensurance
-					if(deathCheck === 3){
+					if(deathCheck === 3) {
 						return true;
 					}
 				}
@@ -102,29 +105,34 @@ Monster.prototype.move = function() {
 	var relativePos_x = this._stage.getActor(nNew_x, new_y);
 	var relativePos_y = this._stage.getActor(new_x, nNew_y);
 
+	/*
 	//Tell player that they are dead
 	if (other_actor === this._stage.player) {
 		this._stage.player.killed = true;
 	}
 
+
 	//Don't allow player to move if they collide with a sticky box
 	if (other_actor instanceof StickyBox){
 		return false;
 	}
+	*/
 
 	//Ricochet collision
 	if (other_actor !== null) {
+		//TODO(SLatychev): Case hit corner, and gets blocked from behind
 		if (relativePos_x !== null) {
 			this.dy = -this.dy;
 		}
 		if (relativePos_y !== null) {
 			this.dx = -this.dx;
 		}
+		return true; 
 	}
 	
 	this._actor.setPosition(monster_pos[0]+this.dx, monster_pos[1]+this.dy);
 
-	return false;
+	return true;
 }
 
 
